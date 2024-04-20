53cb34ede96cb5ab66171639804c76e2e8871d1d
andrewtroicki <atroicki@icloud.com>
2024-04-20 11:48:47 +1000

    tests passed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, driverName) {
+    return drivers.filter((driver) => driver.toLowerCase() === driverName.toLowerCase());
+}
+// slice() does not change original array, non destructive
+function fuzzyMatch (drivers, driverName) {
+    return drivers.filter((driver) => driver.slice(0,2) === driverName.slice(0,2))
+}
+
+function matchName (drivers, driverName) {
+    return drivers.filter((driver) => driver.name === driverName)
+}
